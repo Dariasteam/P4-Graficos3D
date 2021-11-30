407e4b343959263c0a11cad9990d9bd0e17530d4
darias <dariasteam94@gmail.com>
2021-11-30 22:41:47 +0100

    Controlando llamadas a callbacks de escena sin implementar
    Former-commit-id: 24b0c74c7de7588eeb2ccc237537563300d4ea25

--- src/scene/Scene.hpp
+++ src/scene/Scene.hpp
@@ -20,13 +20,33 @@ public:
   static Scene* generate_default ();
   static Scene* generate_scene_2 ();
 
-  std::function<void (void)> init = [](void){};
-  std::function<void (int width, int height)>  on_resize = [](int, int){};
-  std::function<void (unsigned char key)>  on_keyboard = [](unsigned char){};
-  std::function<void (int button, int state, int x, int y)> on_mouse_button  = [](int, int, int, int){};
-  std::function<void (int x, int y)> on_mouse_motion  = [](int, int){};
-  std::function<void (void)> on_idle = [](void){};;
-  std::function<void (void)> render = [](void){};;
+  std::function<void (void)> init = [](void) {
+    std::cerr << "Warning: Llamada a 'init' sin implementar" <<std::endl;
+  };
+
+  std::function<void (int width, int height)>  on_resize = [](int, int) {
+    std::cerr << "Warning: Llamada a 'on_resize' sin implementar" <<std::endl;
+  };
+
+  std::function<void (unsigned char key)>  on_keyboard = [](unsigned char) {
+    std::cerr << "Warning: Llamada a 'on_keyboard' sin implementar" <<std::endl;
+  };
+
+  std::function<void (int button, int state, int x, int y)> on_mouse_button = [](int, int, int, int) {
+    std::cerr << "Warning: Llamada a 'on_mouse_button' sin implementar" <<std::endl;
+  };
+
+  std::function<void (int x, int y)> on_mouse_motion = [](int, int) {
+    std::cerr << "Warning: Llamada a 'on_mouse_motion' sin implementar" <<std::endl;
+  };
+
+  std::function<void (void)> on_idle = [](void){
+    std::cerr << "Warning: Llamada a 'on_idle' sin implementar" <<std::endl;
+  };
+
+  std::function<void (void)> render = [](void) {
+    std::cerr << "Warning: Llamada a 'render' sin implementar" <<std::endl;
+  };
 };
 
 #endif //_SCENE_H_
